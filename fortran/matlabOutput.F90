11f1e2c91bfb21755e9d31dc3013be2f3746495a
daringli <bstefan@chalmers.se>
2015-10-19 15:04:32 +0200

    Added notes in matlabOutput.F90 to indicate that that subroutine takes a long long time to run, and made it more verbose to give the user feedback of its progress.

--- fortran/matlabOutput.F90
+++ fortran/matlabOutput.F90
@@ -12,9 +12,11 @@ contains
 
   ! *********************************************************
   ! Create a PETSc viewer to record output for Matlab
+  ! Note: MatView calls may take a very long time to complete,
+  ! As an example a 13 second simulation took 650 seconds to
+  ! Write, and the file size ended up being 110MB.
   ! *********************************************************
   subroutine writeMatlabOutput(soln,time1)
-
     Vec, intent(in) :: soln
 
     PetscErrorCode :: ierr
@@ -22,12 +24,16 @@ contains
     PetscLogDouble :: time2
     PetscViewer MatlabOutputViewer
 
+       print *,"[",myCommunicatorIndex,"] Creates Matlab output. Note that this may take a very long time."
+
     call PetscViewerASCIIOpen(MPIComm, &
          & MatlabOutputFilename,&
          & MatlabOutputViewer, ierr)
     CHKERRQ(ierr)
+
     call PetscViewerSetFormat(MatlabOutputViewer, PETSC_VIEWER_ASCII_MATLAB, ierr)
     CHKERRQ(ierr)
+
 
     call PetscObjectSetName(rhs, "rhs", ierr)
     CHKERRQ(ierr)
@@ -43,11 +49,14 @@ contains
        CHKERRQ(ierr)
        call MatView(preconditionerMatrix, MatlabOutputViewer, ierr)
        CHKERRQ(ierr)
+          print *,"[",myCommunicatorIndex,"] Done writing preconditioner matrix. Start writing the main matrix..."
     end if
     call PetscObjectSetName(matrix, "matrix", ierr)
     CHKERRQ(ierr)
     call MatView(matrix, MatlabOutputViewer, ierr)
     CHKERRQ(ierr)
+
+
 
     call PetscTime(time2, ierr)
     if (masterProcInSubComm) then
