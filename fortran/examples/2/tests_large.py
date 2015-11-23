8420cb6227899472d21ee7c33236452fde07ebca
John Omotani <omotani@chalmers.se>
2015-11-23 14:08:31 +0100

    Test with makeLocalApproximation=.false.
    Added a test ('examples/2/') using makeLocalApproximation=false (otherwise the
    same as 'examples/1/', except that it also uses leftBoundaryScheme=2 and
    rightBoundaryScheme=2) so that all the terms in the DKE are tested.
    
    Updated the makefile so that 'make test' runs both large and small tests, and
    added a new target 'shorttest' that runs only the small tests ('examples/1/' at
    present).
    
    Added a new function to testsCommon (called 'diffAll') that checks the absolute
    error in all the output arrays. I used absolute rather than relative since
    variables that happen to have a small value may fail a relative error check unless
    the solverTolerance is very small.

---
+++ fortran/examples/2/tests_large.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# This python script checks the perfectOutput.h5 file for an example to 
+# see if the results are close to expected values.  This script may be
+# run directly, and it is also called when "make test" is run from the
+# main PERFECT directory.
+
+execfile('../testsCommon.py')
+
+desiredRelativeTolerance = 0.001
+desiredAbsoluteTolerance = 0.0001
+
+numFailures = 0
+
+numFailures += shouldBe("run  1/BHat[1,1;;;]", 0.874211, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/THat[25,0;;;]", 0.920468, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/d(PhiHat)d(psi)[1;;;]", 0.00178617, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/d(nHat)d(psi)[24,1;;;]", -0.827984, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/FSABFlow[0,0;;;]", 16.6683, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/FSABFlow[0,1;;;]", 11.8187, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/densityPerturbation[2,1,0;;;]", -0.00132427, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/densityPerturbation[2,1,1;;;]", -0.000325815, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/flow[16,7,0;;;]", 15.4452, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/flow[16,7,1;;;]", 13.291, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/heatFlux[6,0;;;]", -1.57664, desiredRelativeTolerance)
+numFailures += shouldBe("run  1/heatFlux[6,1;;;]", -0.376746, desiredRelativeTolerance)
+numFailures += diffAll(desiredAbsoluteTolerance)
+
+exit(numFailures > 0)
