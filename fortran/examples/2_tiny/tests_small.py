cfd8983421a2aea03b6101373582779a54a84f71
bs <nope>
2016-12-06 14:14:15 +0100

    Added some useful extras to new examples.

---
+++ fortran/examples/2_tiny/tests_small.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# This python script checks the perfectOutput.h5 file for an example to 
+# see if the results are close to expected values.  This script may be
+# run directly, and it is also called when "make test" is run from the
+# main PERFECT directory.
+
+execfile('../testsCommon.py')
+
+desiredRelativeTolerance = 0.001
+desiredAbsoluteTolerance = 0.0001
+
+numFailures = 0
+
+numFailures += diffAll(desiredAbsoluteTolerance)
+
+exit(numFailures > 0)
