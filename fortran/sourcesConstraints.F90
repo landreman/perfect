69ea05def29d5186a721fff1cdfed4b5953a542d
daringli <bstefan@hebbe.int.private>
2017-01-11 17:50:15 +0100

    Added a new source species structure from Hebbe to relevant sources (speciesIndep and extra)

--- fortran/sourcesConstraints.F90
+++ fortran/sourcesConstraints.F90
@@ -319,7 +319,7 @@ contains
        			 * nHats(:,1)/charges(1:Nspecies)
     
     case default
-       print *,"Error! Invalid extraSourcesSpeciesStructure. Currently supported values are: 0,1,2,3."
+       print *,"Error! Invalid extraSourcesSpeciesStructure. Currently supported values are: 0,1,2,3,4."
        stop
     end select
 
@@ -497,8 +497,11 @@ contains
        sourceSpeciesPart = masses(1:Nspecies)*nHats(:,1)
     case(3)
        sourceSpeciesPart = nHats(:,1)*masses(1:Nspecies)**(1.5)/charges(1:Nspecies)
+   case(4)
+       sourceSpeciesPart = masses(1:Nspecies) &
+       			 * nHats(:,1)/charges(1:Nspecies) 
     case default
-       print *,"Error! Invalid speciesIndepSourcesSpeciesStructure. Currently supported values are: 0,1,2,3."
+       print *,"Error! Invalid speciesIndepSourcesSpeciesStructure. Currently supported values are: 0,1,2,3,4."
        stop
     end select
 
