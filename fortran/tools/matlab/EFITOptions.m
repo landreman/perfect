a91e423664db571566a7d503dd2ed42e562b8b3d
John Omotani <omotani@chalmers.se>
2017-02-09 11:14:46 +0100

    EFIT reader
    Made copy of read_eqdsk.m from 'matlab/' directory to
    'fortran/tools/matlab/' and in the 'fortran/tools/matlab' version
    commented out lines that check whether switch=3: they appear not to be
    required as EFIT files from machines such as ASDEX, MAST and JET have
    switch=0 but still read OK.

--- fortran/tools/matlab/EFITOptions.m
+++ fortran/tools/matlab/EFITOptions.m
@@ -1,7 +1,7 @@
 % EFIT options for generateGeometryFromEFIT_script.m
 EFITFilename = 'g128913.01500_65x65';
 topCropZ = 1.6;
-bottomCropZ = 1.6;
+bottomCropZ = -topCropZ;
 innerCropR = 0.0;
 outerCropR = 5.0;
 smoothInPsi = true;
